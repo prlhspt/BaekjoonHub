52c4a72182314851d1683e04e1403a3f86d20e6b
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-04-03 21:38:07 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,6 +1,6 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,27 @@
 class Solution {
+
+    static int count;
+    static int target;
+
+    void dfs(int[] arr, int sum, int index) {
+        if (index == arr.length) {
+            if (sum == target) {
+                count++;
+            }
+            return;
+        }
+        
+        dfs(arr, sum + arr[index], index+1);
+        dfs(arr, sum - arr[index], index+1);
+
+    }
+
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        return answer;
+
+        this.target = target;
+
+        dfs(numbers, 0, 0);
+
+        return count;
     }
 }
