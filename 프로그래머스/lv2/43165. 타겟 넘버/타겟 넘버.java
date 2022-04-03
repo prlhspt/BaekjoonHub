ba907cb3182083167bed8cb2e0a538401fa27cac
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-04-03 21:37:21 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,6 @@
 class Solution {
-
-    static int count;
-    static int target;
-
-    void dfs(int[] arr, int sum, int index) {
-        if (index == arr.length) {
-            if (sum == target) {
-                count++;
-            }
-            return;
-        }
-        
-        dfs(arr, sum + arr[index], index+1);
-        dfs(arr, sum - arr[index], index+1);
-
-    }
-
     public int solution(int[] numbers, int target) {
-
-        this.target = target;
-
-        dfs(numbers, 0, 0);
-
-        return count;
+        int answer = 0;
+        return answer;
     }
 }
