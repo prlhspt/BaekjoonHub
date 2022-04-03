9b2068446da236409174b87d184427d8196a6005
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-04-03 20:07:12 +0900

    [level 3] Title: 단속카메라, Time: 24.53 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
@@ -1,58 +1,64 @@
 /**
-
-모든 차량이 단속용 카메라 한번은 만나게
-
-routes[i][0] i번째 차량 고속도로 진입한 지점
-routes[i][1] i번째 차량 나간 지점
-
--5 지점에 카메라를 설치하면 2, 4
--15 지점에 카메라를 설치하면 1, 3
-
--20 ~ -15
-
--14 ~ -5
-
--18 ~ -13
-
--5 ~ -3
-
--15에 설치하면 1, 3 커버 됨
-
--5에 설치하면 2, 4 커버 됨
-
-map에 넣고 +1씩 해서 길 보관
-
-map 최대값부터 정렬한다음에 하나씩 꺼내서 카메라 설치하고 그 구간 다 -1씩 하면 됨
-
-*/
+ end가 빠른 순서대로 설치하면 됨
+ */
 
 import java.util.*;
 import java.util.stream.*;
 
 class Solution {
+
+    class Road implements Comparable<Road> {
+        private int start;
+        private int end;
+
+        public Road (int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getStart()  {
+            return this.start;
+        }
+
+        public int getEnd() {
+            return this.end;
+        }
+
+        @Override
+        public int compareTo(Road o) {
+            return (this.end < o.end) ?
+                    -1 : ((this.end == o.end) ?
+                    0 : 1);
+        }
+
+    }
+
     public int solution(int[][] routes) {
-        
-        List<List<Integer>> list = Arrays.stream(routes)
-            .map(u -> Arrays.stream(u)
-                .boxed()
-                .collect(Collectors.toList()))
-            .collect(Collectors.toList());
-        
-        Map<Integer, Integer> map = new HashMap();
-        
-        for (int a = 0; a < routes.length; a++) {
-            for (int b = routes[a][0]; b < routes[a][1]; b++) {
-                map.merge(b, 1, (i, j) -> (i + j));
+        int answer = 0;
+
+        List<Road> list = new ArrayList<>();
+
+        for (int i = 0; i < routes.length; i++) {
+            Road road = new Road(routes[i][0], routes[i][1]);
+            list.add(road);
+        }
+        Collections.sort(list);
+
+        // 하나 꺼내서 하나 설치하고, 여기에 찍힐 수 있는 차 전부 제거하기
+        while (!list.isEmpty()) {
+            Road road = list.get(0);
+            list.remove(0);
+            answer++;
+
+            int index = 0;
+            while (index < list.size()) {
+                if (list.get(index).getStart() <= road.getEnd() && list.get(index).getEnd() >= road.getStart()) {
+                    list.remove(index);
+                } else {
+                    index++;
+                }
             }
         }
-        
-        Map<Integer, Integer> sortedMap = map.entrySet().stream()
-            .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-            .collect(Collectors.toMap(k -> k.getKey(), v -> v.getValue(),
-                        (e1, e2) -> e1, () -> new LinkedHashMap<>()));
-            
-        System.out.print(sortedMap);
-        
-        return 0;
+        return answer;
     }
 }
