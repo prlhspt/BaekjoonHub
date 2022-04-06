0ab4962c7754312ac354beb8efeb28cfddf4b58a
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-04-06 12:03:23 +0900

    [level 3] Title: 입국심사, Time: 0.36 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.java
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -1,65 +1,42 @@
-// 10억번이면 N에 풀기에도 어려우므로 이분탐색 고려
-
 /*
-시작 시 바로 2명 출발
-7분 됐을 때 1명 출발
-10분 됐을 때 1명 출발
-14분 됐을 때 1명 출발
-21분 됐을 때 1명 출발(20분 아님)
-28분에 끝
+결정 문제 : 심사에 걸리는 시간을 mid라고 했을 때 n명이 times안에 심사할 수 있는가?
+Check(mid) = F -> F -> ... -> T -> T
 
-어떻게 n이랑 times만 가지고 28을 이분탐색?
+Checklist
+1. Check(lo) = F, Check(hi) = T를 만족하는가?
+2. lo, hi는 정답이 될 수 있는 모든 범위를 나타낼 수 있는가?
+ */
 
-10억을 이분탐색 야무지게 잘라서 조건 탐색하는 방법 없을까?
-
-10억과 무언가를 비교해서 무언가 < 10억 이면 
-무언가 < 10억/2 가 돼야 하는데,,
-*/
-
-/**
-힌트
-처음에 middle 값을 대충 정함
-middle 시간안에 처리할 수 있는 총 사람수를 구함
-[해당시간에 처리할수 있는 사람수]와 [목표 사람수]를 비교함
-*/
-
-/**
-입국 심사를 기다리는 사람
-n < 1,000,000,000
-
-심사관이 심사하는데 걸리는 시간 배열
-times.length < 100,000
-*/
-
+import java.math.BigInteger;
 import java.util.*;
 
 class Solution {
 
-    public long solution(int n, int[] times) {
+    boolean check(int[] arr, long mid, int target) {
+        long sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += (mid / arr[i]);
+        }
+        return sum >= target;
+    }
 
-        long answer = 0;
-        
+
+    public long solution(int n, int[] times) {
         Arrays.sort(times);
-        
-        long left = 0;
-        long right = (long) n * times[times.length - 1];
-        
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            
-            long people = 0;
-            for (int i = 0; i < times.length; i++) {
-                people += (mid / times[i]);
-            }
-            
-            if (n > people) {
-                left = mid + 1;
+
+        long low = -1;
+        long high = (long) n * (long) times[times.length - 1] + 1;
+
+        // low와 high 사이에는 서로 다른 칸이 존재한다. (F -> T가 되는 경계)
+        while (low + 1 < high) {
+            long mid = low + (high - low) / 2;
+
+            if (check(times, mid, n)) {
+                high = mid;
             } else {
-                right = mid - 1;
-                answer = mid;
+                low = mid;
             }
         }
-        
-        return answer;
+        return high;
     }
 }
