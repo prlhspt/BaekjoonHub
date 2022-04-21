7c76386e3d9e5eb8c0e40bb370eb14ac12fa4355
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-04-21 13:01:23 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.16 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -9,47 +9,27 @@
 import java.util.*;
 
 class Solution {
+    
+    public int findMaxSum(int from, int to, int[] sticker) {
+    int[] sumArr = new int[sticker.length - 1];
 
-    int[] reverseArray(int[] arr) {
-        int temp = 0;
-        for (int i = 0; i < arr.length / 2; i++) {
-            temp = arr[i];
-            arr[i] = arr[arr.length-i-1];
-            arr[arr.length-i-1] = temp;
-        }
-        return arr;
+    sumArr[0] = sticker[from];
+    sumArr[1] = Math.max(sticker[from + 1], sticker[from]);
+    for (int i = from + 2, sumIndex = 2; i <= to; i++, sumIndex++) {
+        sumArr[sumIndex] = Math.max((sticker[i] + sumArr[sumIndex - 2]), sumArr[sumIndex - 1]);
+    }
+
+    return sumArr[sumArr.length - 1];
+
     }
 
     public int solution(int sticker[]) {
-
         if (sticker.length == 1) {
             return sticker[0];
+        } else if(sticker.length == 2) {
+            return Math.max(sticker[0], sticker[1]);
+        } else {
+            return Math.max(findMaxSum(0, sticker.length - 2, sticker), findMaxSum(1, sticker.length - 1, sticker));
         }
-
-        int answer = 0;
-
-        int[] ansArr = new int[sticker.length];        
-
-        // i[0]까지의 최댓값 + i[2]하고, i[1]까지의 최댓값 중 더 큰거 고르면 됨
-
-        ansArr[0] = sticker[0];
-        ansArr[1] = Math.max(ansArr[0], sticker[1]);
-        for (int i = 2; i < sticker.length - 1; i++) {
-            ansArr[i] = Math.max(ansArr[i-2] + sticker[i], ansArr[i-1]);
-        }
-
-        int[] revArr = new int[sticker.length];
-        reverseArray(sticker);
-
-        revArr[0] = sticker[0];
-        revArr[1] = Math.max(revArr[0], sticker[1]);
-
-        for (int i = 2; i < sticker.length - 1; i++) {
-            revArr[i] = Math.max(revArr[i-2] + sticker[i], revArr[i-1]);
-        }
-
-        answer = Math.max(ansArr[ansArr.length-2], revArr[revArr.length-2]);
-
-        return answer;
     }
 }
