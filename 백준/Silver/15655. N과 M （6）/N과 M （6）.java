e8257c80dde61fddb3528f5f1152758f7dd29491
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-05-06 01:34:58 +0900

    [Silver III] Title: N과 M (6), Time: 144 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -5,25 +5,27 @@ public class Main {
     
     static int n, m;
     static int[] arr;
-    static int[] res;
+    static int[] num;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
     
-    public static void func(int k, int idx) {
+    public static void func(int k) {
         if (k == m) {
             for (int i = 0; i < m; i++)
-                sb.append(res[i] + " ");
+                sb.append(num[arr[i]] + " ");
             sb.append("\n");
             return;
         }
         
-        for (int i = idx; i < n; i++) {
+        int st = 0;
+        if (k != 0) st = arr[k-1] + 1;
+        for (int i = st; i < n; i++) {
             if (visited[i]) continue;
             
-            res[k] = arr[i];
+            arr[k] = i;
             
             visited[i] = true;
-            func(k+1, i);
+            func(k+1);
             visited[i] = false;
             
         }
@@ -38,18 +40,18 @@ public class Main {
         m = Integer.parseInt(s[1]);
         
         arr = new int[n];
-        res = new int[n];
+        num = new int[n];
         visited = new boolean[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            num[i] = Integer.parseInt(st.nextToken());
         }
         
-        Arrays.sort(arr);
+        Arrays.sort(num);
         
-        func(0, 0);
+        func(0);
         
         System.out.print(sb);
 
