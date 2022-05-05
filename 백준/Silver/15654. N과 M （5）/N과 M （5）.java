a20f6ab16670df466854ca4c6873390d41ce175f
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-05-06 01:12:16 +0900

    [Silver III] Title: N과 M (5), Time: 420 ms, Memory: 41236 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static int n, m;
+    static int[] arr;
+    static int[] res;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++)
+                sb.append(res[i] + " ");
+            sb.append("\n");
+            return;
+        }
+        
+        int prev = k - 1;
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            
+            res[k] = arr[i];
+            
+            visited[i] = true;
+            func(k+1);
+            visited[i] = false;
+            
+        }
+    }
+      
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] s = br.readLine().split(" ");
+        
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+        
+        arr = new int[n];
+        res = new int[n];
+        visited = new boolean[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+        func(0);
+        
+        System.out.print(sb);
+
+  }
+}
