2abc04dcb4fa4c98f0f7bf682c22f2024fb826a0
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-05-06 14:16:33 +0900

    [Silver II] Title: N과 M (11), Time: 452 ms, Memory: 84912 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -4,27 +4,26 @@ import java.io.*;
 public class Main {
     
     static int n, m;
-    static int[] arr;
-    static int[] num;
+    static int[] arr = new int[10];
+    static int[] num = new int[10];
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
     
     public static void func(int k) {
         if (k == m) {
-            for (int i = 0; i < k; i++)
-                sb.append(arr[i] + " ");
+            for (int i = 0; i < m; i++)
+                sb.append(num[arr[i]] + " ");
             sb.append("\n");
             return;
         }
         
-        int tmp = 0;
         for (int i = 0; i < n; i++) {
-            if (num[i] == tmp) continue;
-            
-            arr[k] = num[i];
-            tmp = num[i];
+            arr[k] = i;
             func(k+1);
         }
     }
+    
+    static boolean[] chk = new boolean[10002];
       
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,18 +32,24 @@ public class Main {
         
         n = Integer.parseInt(readString[0]);
         m = Integer.parseInt(readString[1]);
-        
-        arr = new int[n];
+    
         num = new int[n];
+        visited = new boolean[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        for (int i = 0; i < n; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+        int idx = 0;
+        while ((n--) > 0) {
+            num[idx] = Integer.parseInt(st.nextToken());
+            if (chk[num[idx]]) continue;
+            chk[num[idx]] = true;
+            idx++;
         }
         
-        Arrays.sort(num);
+        // System.out.println(Arrays.toString(num));
         
+        n = idx;
+        Arrays.sort(num, 0, 0+n);
         func(0);
         
         System.out.println(sb);
