41a1657d5b14e4a7fb9981a9cdc16dfa6dc9513a
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-06-24 18:56:13 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.11 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,47 +1,60 @@
-/**
-진법 변환 한다음에 하나씩 돌면서 조건 걸러내면 될 듯
-
-*/
+import java.util.*;
 
 class Solution {
-    
-    public boolean isPrime(long num) {
-        
+
+    public boolean primeCheck(String s) {
+        long num = Long.parseLong(s);
+
         if (num < 2) {
             return false;
+        } else if (num == 2) {
+            return true;
+        } else if (num % 2 == 0) {
+            return false;
         }
-        
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+
+        for (int i = 3; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
         }
-        
         return true;
     }
     
+    private boolean Extraction(Deque<String> deque, StringBuilder sb) {
+        while (!deque.isEmpty() && !deque.peekLast().equals("0")) {
+            sb.insert(0, deque.pollLast());
+        }
+
+        if (!sb.toString().equals("") && primeCheck(sb.toString())) {
+            return true;
+        }
+        return false;
+    }
+
     public int solution(int n, int k) {
 
         int answer = 0;
-        
-        String s = Integer.toString(n, k);
-        
-        // 211 0 2 0 1 0 1 0 11
-        // 0 만나면 그 전까지 수들 처리해주는 아이디어도 좋은듯
-        int start = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '0') {
-                if (isPrime(Long.parseLong(s.substring(start, i)))) {
+
+        String notation = Integer.toString(n, k);
+
+        Deque<String> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < notation.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            if (notation.charAt(i) == '0') {
+                if (!deque.isEmpty() && Extraction(deque, sb)) {
                     answer++;
                 }
-                start = i;
-            }   
+            }
+            deque.offer(String.valueOf(notation.charAt(i)));
         }
-        
-        if (isPrime(Long.parseLong(s.substring(start, s.length())))) {
+
+        StringBuilder sb = new StringBuilder();
+        if (!deque.isEmpty() && Extraction(deque, sb)) {
             answer++;
-        }    
-        
+        }
+
         return answer;
     }
 }
