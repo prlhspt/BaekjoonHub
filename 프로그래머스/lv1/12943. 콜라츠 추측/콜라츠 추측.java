adb9787f3b5940a317626656dc89093493bf2f74
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-06-25 18:03:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        for (int i = 0; i <= 500; i ++) {
+            if (num == 1) {
+                return answer;
+            }
+            if (num % 2 == 0) {
+                num /= 2;
+                answer++;
+            } else if (num % 2 == 1) {
+                num = num * 3 + 1;
+                answer++;
+            }
+        }
+        return -1;
+    }
+}
